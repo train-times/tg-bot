2cdde2d40738c3ac541afe0b62ca3e876617a5d7
Roman <dev@rman.dev>
2026-09-24 10:20:09 +0100

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -5,6 +5,9 @@ export const HELP_MESSAGE = `
 *Examples*
 
 ${"`/schedule BSK WIN`"}  —  From Basingstoke to Winchester
+${"`/schedule WIN BSK`"}  —  From Winchester to Basingstoke
+${"`/schedule BSK WAT`"}  —  From Basingstoke to London Waterloo
+${"`/schedule WAT BSK`"}  —  From London Waterloo to Basingstoke
 `;
 
 export function handleHelp(
